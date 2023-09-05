17744a20dd7dbd1015348ef964314e75d7e9be30
Shivam Vishwakarma <shivam92211@gmail.com>
2023-09-05 15:29:02 +0530

    second commit

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def load_llm():
     return llm
 
 st.title("Chat with CSV using Llama2 🦙🦜")
-st.markdown("<h3 style='text-align: center; color: white;'>Built by <a href='https://github.com/AIAnytime'>AI Anytime with ❤️ </a></h3>", unsafe_allow_html=True)
 
 uploaded_file = st.sidebar.file_uploader("Upload your Data", type="csv")
 
